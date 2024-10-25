3aff736ad5b04a0a4bf08f613aa58053649a99f8
Sean Carlyle <808jalapeno@gmail.com>
2024-10-24 23:27:28 -0700

    adding vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,8 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 
 export default {
-  preprocess: [vitePreprocess()],
-  kit: {
-	adapter: adapter()
+	kit: {
+		adapter: adapter({
+		})
 	}
 };
