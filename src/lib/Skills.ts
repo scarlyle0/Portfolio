4c8e824788cbade33e24015660bc81fd9661fee4
Sean Carlyle <808jalapeno@gmail.com>
2024-10-24 20:12:34 -0700

    first commit

---
+++ src/lib/Skills.ts
@@ -0,0 +1,9 @@
+const skills = {
+	'programming languages': ['JavaScript', 'Python', 'GDScript', 'C', 'C++', 'SQL'
+	],
+	'markup and styling languages': ['HTML', 'CSS'],
+	'frameworks and libraries': ['React.js', 'Express.js', 'SvelteKit', 'Three.js'],
+	'database management systems': ['PostgreSQL'],
+	'other technologies': ['Node.js', 'Git', 'Docker', 'Odoo', 'OpenGL', 'Godot', 'Unity', 'Selenium']
+};
+export default skills;
---
+++ src/routes/+page.svelte
@@ -0,0 +1,142 @@
+<script>
+	import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
+	import FaGithub from 'svelte-icons/fa/FaGithub.svelte';
+	import FaEnvelope from 'svelte-icons/fa/FaEnvelope.svelte';
+	import { modalOpened } from '$lib/store';
+</script>
+
+<svelte:head>
+	<title>Sean Carlyle - Main page</title>
+</svelte:head>
+<main>
+	<div id = "intro">
+		<img src="portrait.jpg" alt="pfp">
+		<div>
+			<h1>Sean Carlyle</h1>
+			<h2>Software Developer</h2>
+		</div>
+	</div>
+	<div class="icons">
+		<div
+			role="button"
+			tabindex="0"
+			on:keypress={() => {
+				modalOpened.set(true);
+			}}
+			on:click={() => {
+				modalOpened.set(true);
+			}}
+		>
+			<div class="icon">
+				<FaEnvelope />
+			</div>
+		</div>
+
+		<a
+			href="https://github.com/scarlyle0"
+			aria-label="GitHub"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<div class="icon">
+				<FaGithub />
+			</div>
+		</a>
+		<a
+			href="https://www.linkedin.com/in/sean-carlyle/"
+			aria-label="Linkedin"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<div class="icon">
+				<FaLinkedin />
+			</div>
+		</a>
+	</div>
+</main>
+
+<style>
+	
+	#intro {
+		display:flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		gap: 50px;
+
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+	}
+	main {
+		text-align: center;
+		padding: 0;
+		margin: 0 auto;
+		text-align: center;
+
+		display: flex;
+		flex-direction: column;
+		height: calc(100vh - 80px - 88px);
+		justify-content: center;
+		align-items: center;
+	}
+
+	h1 {
+		font-weight: 700;
+	}
+
+	main > h1 {
+		margin: 50px 10px 0;
+		font-size: 36px;
+	}
+
+	.icons {
+		display: flex !important;
+		justify-content: center !important;
+		align-items: center;
+		gap: 20px;
+		cursor: pointer;
+		font-size: 30px;
+		display: flex;
+		justify-content: space-between;
+		max-width: 200px;
+		margin: 50px auto 0;
+	}
+
+	.icon {
+		cursor: pointer;
+		transition: color 0.2s ease-in-out;
+		width: 40px;
+	}
+	.icon:hover {
+		color: #ca3c25;
+	}
+
+	@media (min-width: 900px) {
+		main > h1 {
+			font-size: 48px;
+		}
+	}
+
+	@media (min-width: 600px) {
+		main {
+			max-width: none;
+		}
+	}
+	h1 {
+		font-weight: 700;
+		margin: 0px 0; /* Adjust margin here */
+	}
+
+	h2 {
+		margin: 0; /* Remove margin to reduce space */
+	}
+
+	img {
+		border-radius: 50%;
+		width: 256px;
+		height: 256px;
+	}
+</style>
