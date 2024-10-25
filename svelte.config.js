0beddf7a3e6605221691f1ba12c3591d13cc99a2
Sean Carlyle <808jalapeno@gmail.com>
2024-10-24 23:30:47 -0700

    adding back preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-vercel';
 
 export default {
-	kit: {
-		adapter: adapter({
-		})
+  preprocess: [vitePreprocess()],
+  kit: {
+	adapter: adapter()
 	}
 };
